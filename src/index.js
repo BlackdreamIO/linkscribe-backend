cbb84014d85b7fc157730e5621f85dea1cd8ce60
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2024-10-04 17:12:31 +0600

    porject iit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import compression from "compression";
+import router from "./router/router.js";
+
+const allowedOrigins = ["*", "http://localhost:5173"];
+const port = 5000;
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+};
+
+const app = express();
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(compression());
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log(`server is running at http://localhost:${port}`);
+});
